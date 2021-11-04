71c472812e3cbb0c99354cafc097ec1f996ab5fe
Peter Yakovlev <gorkiy@gmail.com>
2021-11-04 12:16:32 +0300

    Add basic properties and method

--- solution/index.js
+++ solution/index.js
@@ -1,3 +1,30 @@
-module.exports = class MySet {
-    // реализация
+class MySet {
+    constructor(array) {
+        console.log('array: ', array);
+        this._data = this.getUniqueValues(array);
+    }
+
+    _data = [];
+
+    *[Symbol.iterator]() {
+        for (let value of this._data) {
+            yield value;
+        }
+    }
+
+    getUniqueValues(array) {
+        const map = {};
+
+        return array.filter(el => {
+            if (map[el]) return false;
+
+            map[el] = true;
+            return el;
+        })
+    }
 }
+// тесты
+const set = new MySet([4, 8, 15, 15, 16, 23, 42]);
+
+// хранит только уникальные значения
+console.log([...set]); // [ 4, 8, 15, 16, 23, 42 ]
